25747cd2eca596cf13f246af9fab369e6bf511b5
SanBran <luminoforos@gmail.com>
2023-10-02 15:29:31 -0600

    Deploy DB

--- back/server.js
+++ back/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const { conn } = require("./src/DB_connection");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const server = express();
 

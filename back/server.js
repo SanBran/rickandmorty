53253dad513e4137f3f48979ddbabc4ba0fb23a9
SanBran <luminoforos@gmail.com>
2023-10-02 16:09:30 -0600

    Deploy DB

--- back/server.js
+++ back/server.js
@@ -11,7 +11,24 @@ const server = express();
 
 server.use(express.json());
 server.use(morgan("dev"));
-server.use(cors());
+server.use(
+  cors({
+    origin: "*",
+  })
+);
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+server.use(express.urlencoded({ extended: true }));
 
 server.use("/", router);
 

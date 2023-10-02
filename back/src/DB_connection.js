8c6089b86e30ef4bc9b83cd892228dbae6e1d339
SanBran <luminoforos@gmail.com>
2023-10-02 16:27:30 -0600

    Deploy DB

--- back/src/DB_connection.js
+++ back/src/DB_connection.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+const {pg} = require('pg')
 const { Sequelize } = require("sequelize");
 const UserModel = require("./models/User");
 const FavoriteModel = require("./models/Favorite");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectModule: pg,
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
